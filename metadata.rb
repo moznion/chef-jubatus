9807ad0786f62bb15697194274b3084f4edddc6f
moznion <moznion@gmail.com>
2013-04-19 15:29:51 +0900

    Add some environment files.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name             'jubatus'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'moznion'
+maintainer_email 'moznion@gmail.com'
+license          'MIT'
 description      'Installs/Configures jubatus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.1'
+
+depends 'apt'
+
+%w{ ubuntu debian redhat centos fedora }.each do |os|
+  supports os
+end
